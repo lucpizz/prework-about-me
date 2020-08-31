01fd4f1c5125820949ac85c6a1a9168805fc9d84
lucpizz <Luca.Pizzoferrato@Outlook.com>
2020-08-31 18:40:29 -0400

    adding Module-6

---
+++ Module-6/javascript.js
@@ -0,0 +1,23 @@
+function myFunction1() {
+
+document.getElementById("box").style.height = "250px";
+
+}
+
+function myFunction2() {
+
+  document.getElementById("box").style.background = "blue";
+  
+  }
+
+  function myFunction3() {
+
+    document.getElementById("box").style.opacity = "0";
+    
+    }
+
+function myFunction4() {
+
+  document.getElementById("box").style = "height:150px; width:150px; background-color:orange; margin:25px";
+  
+  }
